62e4966672cc8a97a3187cd44eae7b8aeb1f8217
Richsone-dev <Richsone>
2026-04-29 19:05:53 +0000

    CSS resolution

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    // On force l'utilisation de PostCSS pour éviter les erreurs de syntaxe CSS
+    transformer: 'postcss',
+  },
+  build: {
+    // On utilise esbuild pour la minification du CSS au lieu de lightningcss
+    cssMinifier: 'esbuild',
+  }
 })
-
-
