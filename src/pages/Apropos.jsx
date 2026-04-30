b926d112ec8dca32c7998ba0e3f6d41a1608b4d2
Richsone-dev <Richsone>
2026-04-30 21:10:29 +0000

    mainlayout

--- src/pages/Apropos.jsx
+++ src/pages/Apropos.jsx
@@ -127,6 +127,12 @@ const ProblemSolution = ({ colors, cardStyle }) => {
                     <div className="p-4 rounded-4 shadow border-start border-success border-4 glass premium-card" style={cardStyle}>
                         <img src={solution1} alt="Solution" className="mb-3" style={{ width: '40px', height: '40px' }} />
                         <img
+                        src={img1}
+                        className="img-fluid shadow-lg mt-0"
+                        style={{ maxHeight: '500px', objectFit: 'cover', width: '100%' }}
+                        alt="DjagoYelen Interface"
+                    />
+                    <img
                         src={solution1}
                         className="img-fluid shadow-lg mt-0"
                         style={{ maxHeight: '500px', objectFit: 'cover', width: '100%' }}
