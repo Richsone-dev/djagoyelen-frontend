7f2b932a7954516a3d0e841a13f029f722b2899a
Richsone-dev <Richsone>
2026-04-30 03:43:35 +0000

    versel.json regle

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // L'URL de ton serveur Laravel (Backend)
-    baseURL: import.meta.env.VITE_APP_API_URL, 
+    baseURL: import.meta.env.VITE_APP_API_URL , 
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
