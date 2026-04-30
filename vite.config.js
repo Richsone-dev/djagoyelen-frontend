9a166aa8d6ab99de3f25202c58aea8d6bf8a33e7
Richsone-dev <Richsone>
2026-04-30 10:59:52 +0000

    vite.confog.js  modifié

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  
   build: {
-    // On force esbuild et on désactive lightningcss explicitement
-    cssMinifier: 'esbuild', 
-    minify: 'esbuild',
-    outDir: 'dist'
+    outDir: 'dist',
+    minify: 'esbuild'
   },
+
   css: {
-    transformer: 'postcss',
-    lightningcss: false // On lui dit explicitement de ne PAS l'utiliser
+    transformer: 'postcss'
   }
 })
