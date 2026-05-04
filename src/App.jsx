650d676d13663eab1a9dca295d9646f900cca91d
Richsone-dev <Richsone>
2026-05-04 10:31:55 +0000

    Apropos mis a jour2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import MainLayout from '../components/MainLayout';
+import MainLayout from './components/MainLayout';
 import Dashboard from './pages/Dashboard';
 import Transactions from './pages/Transactions';
 import Budgets from './pages/Budgets';
