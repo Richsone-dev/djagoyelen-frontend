a1b5da02273b24f066eeae3735b393789cb867f5
Richsone-dev <Richsone>
2026-05-04 20:05:28 +0000

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
