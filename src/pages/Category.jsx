a5dbd134f684d7bbb7d99cc6e3dd4945f8c8d1ba
Richsone-dev <Richsone>
2026-05-14 13:24:18 +0000

    page categoy v1

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -55,7 +55,7 @@ const Category = () => {
 
     const api = useMemo(() => axios.create({
         baseURL: 'https://djagoyelen-backend.onrender.com/api' || 'http://localhost:8000/api',
-        baseURL: 'http://localhost:8000/api',
+        //baseURL: 'http://localhost:8000/api',
         headers: {
             Accept: 'application/json',
             Authorization: `Bearer ${localStorage.getItem('token')}`
