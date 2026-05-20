3dea9297728bd59619757cb5d9cee01f8af65cce
Richsone-dev <Richsone>
2026-05-20 22:32:07 +0000

    V4 admin

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
     // L'URL de ton serveur Laravel (Backend)
     baseURL: import.meta.env.VITE_APP_API_URL || 'http://localhost:8000/api',
-    baseURL: 'http://localhost:8000/api',
+    //baseURL: 'http://localhost:8000/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
