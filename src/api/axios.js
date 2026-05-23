b682264f248d0c5a1f44d83181a8270accb49c8a
Richsone-dev <Richsone>
2026-05-23 11:15:55 +0000

    prob enregistremt factures

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
     // L'URL de ton serveur Laravel (Backend)
-    baseURL: import.meta.env.VITE_APP_API_URL || 'http://localhost:8000/api',
+    //baseURL: import.meta.env.VITE_APP_API_URL || 'http://localhost:8000/api',
+    baseURL: 'https://djagoyelen-backend.onrender.com/api' || 'http://localhost:8000/api',
     //baseURL: 'http://localhost:8000/api',
     headers: {
         'Content-Type': 'application/json',
